5e497637ff4d363252e4bcc8e6a2bc2bb8641041
vinodpattar <153218046+vinodpattar@users.noreply.github.com>
2024-12-18 14:02:18 +0530

    Add files via upload

---
+++ bday.js
@@ -0,0 +1,5 @@
+function playMusic() {
+    const music = document.getElementById('birthdayMusic');
+    music.play();
+    alert("Laugh, laugh, live, you are a hundred Varusha, be it like this, Harusha Harusha! 🎵");
+}
